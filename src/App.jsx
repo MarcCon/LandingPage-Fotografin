218268e0ee610575af81aec6b747d18560e52e9f
Marc-Constantin Enke <marc.enke@aol.de>
2023-11-17 18:44:52 +0100

    Links

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import StoryGrid from "./components/Storys";
 import Carousel from "./components/Carousel";
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 const App = () => {
   return (
+
+    <Router>
     <div>
       <Navbar />
-      <Carousel />
-      <StoryGrid />
+      <Switch>
+        <Route path="/story1">
+          <Story1 />
+        </Route>
+        <Route path="/story2">
+          <Story2 />
+        </Route>
+        {/* ... weitere Story-Routen */}
+        <Route path="/">
+          {/* Deine Home-Komponente mit den Ankerpunkten */}
+          <div id="ueber-mich">Über mich Abschnitt</div>
+          {/* ... weitere Abschnitte */}
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 };
 
---
+++ src/pages/Story2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Story2 = () => {
+  return (
+    <div>Story2</div>
+  )
+}
+
+export default Story2
