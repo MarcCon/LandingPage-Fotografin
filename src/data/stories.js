ca069fc4718a31234bc7dd99f279a6153c92d37f
Marc-Constantin Enke <marc.enke@aol.de>
2023-12-08 11:28:54 +0100

    TestNeu

--- src/data/stories.js
+++ src/data/stories.js
@@ -5,9 +5,9 @@ const stories = [
     title: "Gartenhochzeit in Leipzig",
     description: "Helene & Joshua",
     images: [
-      { imageUrl: "/public/Helene1.jpg" },
+      { imageUrl: "/src/assets/BilderSeiten/Helene2.jpg },
 
-      { imageUrl: "/src/assets/BilderSeiten/Helene/Helene2.jpg" },
+      { imageUrl: "/src/assets/BilderSeiten/Helene2.jpg" },
       { imageUrl: "/src/assets/BilderSeiten/Helene/Helene4.jpg" },
       { imageUrl: "/src/assets/BilderSeiten/Helene/Helene5.jpg" },
       { imageUrl: "/src/assets/BilderSeiten/Helene/Helene6.jpg" },
