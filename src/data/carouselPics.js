098a78c279cfb85182632034074b5debcd2f2fac
Marc-Constantin Enke <marc.enke@aol.de>
2023-12-09 01:28:28 +0100

    VercelFix

--- src/data/carouselPics.js
+++ src/data/carouselPics.js
@@ -1,12 +1,11 @@
 const originalImages = [
-  "src/assets/BilderSeiten/Bine/Bine11.jpg",
-
-  "/src/assets/BilderSeiten/Helene/Helene15.jpg",
-  "/src/assets/BilderSeiten/JuleMichelle/Jule9.jpg",
-  "/src/assets/BilderSeiten/Nele/Nele8.jpg",
-  "/src/assets/BilderSeiten/Seega/Sega10.jpg",
-  "/src/assets/BilderSeiten/Roxana/Rox5.jpg",
-  "/src/assets/BilderSeiten/Familienfeier/Fam11.jpg",
+  "/BilderSeiten/Bine/Bine11.jpg",
+  "/BilderSeiten/Helene/Helene15.jpg",
+  "/BilderSeiten/JuleMichelle/Jule9.jpg",
+  "/BilderSeiten/Nele/Nele8.jpg",
+  "/BilderSeiten/Seega/Sega10.jpg",
+  "/BilderSeiten/Roxana/Rox5.jpg",
+  "/BilderSeiten/Familienfeier/Fam11.jpg",
 ];
 
 export default originalImages;
