c4123ed9467b9bcbfb7fab951ba4dddb205a2e94
Marc-Constantin Enke <marc.enke@aol.de>
2023-12-11 21:00:45 +0100

    header

--- src/components/HeaderHomePage.jsx
+++ src/components/HeaderHomePage.jsx
@@ -5,7 +5,7 @@ const HeaderHomePage = () => {
     <div className="h-[1000px] bg-[url('/HeaderBildHelene.png')] bg-cover bg-center bg-no-repeat">
       <div className=" grid h-full grid-rows-3	">
         <div></div>
-        <div className=" pl-32 font-serif text-7xl  text-white sm:text-4xl md:text-5xl lg:text-6xl  xl:text-7xl 2xl:text-7xl">
+        <div className=" ml-32 font-serif text-7xl  text-white sm:text-4xl md:text-5xl lg:text-6xl  xl:text-7xl 2xl:text-7xl">
           <h1 className="pb-8 ">Ihre Momente.</h1>
           <h1 className="pb-8 ">Festgehalten für die Ewigkeit.</h1>
           <button className="h-auto w-auto rounded-lg border border-white bg-transparent p-4 px-12 text-xl font-semibold text-white transition duration-500 ease-in-out hover:bg-white hover:text-black">
