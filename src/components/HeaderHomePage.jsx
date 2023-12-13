e301f56daed6099d90f920d12cc66d1f7ad93bd0
Marc-Constantin Enke <marc.enke@aol.de>
2023-12-13 15:51:51 +0100

    ResponsivHeaderFinal

--- src/components/HeaderHomePage.jsx
+++ src/components/HeaderHomePage.jsx
@@ -5,11 +5,14 @@ const HeaderHomePage = () => {
     <div className="h-[500px] bg-[url('/HeaderBildHelene.jpg')] bg-cover bg-center bg-no-repeat sm:h-[500px] md:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[800px]">
       <div className="items-left ml-8 flex h-full flex-col justify-center xl:ml-32 2xl:ml-32">
         <div className="font-serif text-4xl text-white  sm:text-4xl  md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-7xl">
-          <h1 className="pb-8">Ihre Momente.</h1>
-          <h1 className="pb-8">Festgehalten für die Ewigkeit.</h1>
+          <h1 class="leading-snug">
+            Ihre Momente.
+            <br />
+            Festgehalten für Immer.
+          </h1>
         </div>
         <div className=" text-md l:text-3xl font-serif sm:text-xl md:text-xl lg:text-2xl 2xl:text-3xl ">
-          <button className="h-auto w-auto rounded-lg border border-white bg-transparent p-2 px-12   font-semibold text-white transition duration-500 ease-in-out hover:bg-white hover:text-black ">
+          <button className=" mt-8 h-auto w-auto rounded-lg border border-white bg-transparent p-2 px-12   font-semibold text-white transition duration-500 ease-in-out hover:bg-white hover:text-black ">
             Kontakt
           </button>
         </div>
