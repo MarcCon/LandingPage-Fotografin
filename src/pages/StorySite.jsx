08f1fc792b37c0d55c5333f7b60025e32244d918
Marc-Constantin Enke <marc.enke@aol.de>
2024-03-11 21:58:03 +0100

    StorySide Anpassungen und Lightbox

--- src/pages/StorySite.jsx
+++ src/pages/StorySite.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import stories from "../data/stories";
 import HeaderStory from "../components/HeaderStory";
 import { PhotoAlbum } from "react-photo-album";
-import ModalImage from "react-modal-image";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css"; // Importiere den CSS-Stil
 import LegalFooter from "../components/LegalFooter";
 
 const StorySite = () => {
@@ -11,11 +12,20 @@ const StorySite = () => {
   const story = stories.find((story) => story.id === parseInt(id));
   const storyImages = story?.images || [];
 
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const photos = storyImages.map((image, index) => ({
     src: image.imageUrl,
     alt: `Bild ${index + 1}`,
     width: image.width,
     height: image.height,
+  }));
+
+  // Bereite die Slides für die Lightbox vor
+  const slides = photos.map((photo) => ({
+    src: photo.src,
+    alt: photo.alt,
   }));
 
   return (
@@ -34,24 +44,45 @@ const StorySite = () => {
             <PhotoAlbum
               layout="masonry"
               photos={photos}
+              spacing={6}
               columns={(containerWidth) => {
-                if (containerWidth < 400) return 2;
-                if (containerWidth < 1000) return 2;
-                return 3;
+                if (containerWidth < 600) return 2;
+                if (containerWidth < 1000) return 3;
+                return 4;
               }}
-              renderPhoto={({ photo }) => (
-                <div className="mb-2">
-                  <ModalImage
-                    small={photo.src}
-                    large={photo.src}
+              renderPhoto={({ photo, index }) => (
+                <div
+                  className="mb-2 cursor-pointer"
+                  onClick={() => {
+                    setLightboxOpen(true);
+                    setCurrentSlide(index);
+                  }}
+                >
+                  <img
+                    src={photo.src}
                     alt={photo.alt}
-                    hideDownload={true}
-                    hideZoom={true}
-                    showRotate={false}
+                    style={{ width: "100%", display: "block" }}
                   />
                 </div>
               )}
             />
+            {lightboxOpen && (
+              <Lightbox
+                open={lightboxOpen}
+                close={() => setLightboxOpen(false)}
+                slides={slides}
+                currentIndex={currentSlide}
+                // Optional: Konfiguration für die Navigation
+                onNext={() =>
+                  setCurrentSlide((currentSlide + 1) % slides.length)
+                }
+                onPrev={() =>
+                  setCurrentSlide(
+                    (currentSlide - 1 + slides.length) % slides.length,
+                  )
+                }
+              />
+            )}
           </div>
         </div>
       </div>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    global: {}, // Definiert `global` als leeres Objekt, oder du kannst es durch `window` ersetzen
+  },
 });
